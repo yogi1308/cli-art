0e03d8b22e820f5a69caeaebd3622a9618ff6442
Shreetej Hadge <shreetejhadge1308@gmail.com>
2025-11-01 18:09:00 -0700

    converted brightness number of each pixel to an ascii character

--- cli-art.py
+++ cli-art.py
@@ -1,5 +1,11 @@
 # add option for brightness using lighness max(R, G, B) + min(R, G, B) / 2 and luminosity 0.21 R + 0.72 G + 0.07 B
 from PIL import Image
+
+def convert_brightness_to_ascii(value):
+    ascii_chars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+    value = int((value/255)*100)
+    return ascii_chars[int((value/100)*65)]
+
 with Image.open("image.png") as image_file:
     print("Successfully loaded image")
     print(image_file.size)
@@ -20,3 +26,8 @@ with Image.open("image.png") as image_file:
             rgb_values += x
         pixel_brightness_values.append(rgb_values/3)
     print(pixel_brightness_values[0], len(pixel_brightness_values))
+
+    pixel_ascii_char = []
+    for value in pixel_brightness_values:
+        pixel_ascii_char.append(convert_brightness_to_ascii(value))
+    print(pixel_ascii_char[0], len(pixel_ascii_char))
