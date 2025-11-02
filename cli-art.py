a8d987f343302ed07ee8619393ba160bd256cffd
Shreetej Hadge <shreetejhadge1308@gmail.com>
2025-11-01 19:24:13 -0700

    fixed bugs - changed the list structure of all the lists to make them 2d list, fixed image sizing

--- cli-art.py
+++ cli-art.py
@@ -4,30 +4,47 @@ from PIL import Image
 def convert_brightness_to_ascii(value):
     ascii_chars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
     value = int((value/255)*100)
-    return ascii_chars[int((value/100)*65)]
+    return ascii_chars[int((value/100)*(len(ascii_chars)-1))]
 
 with Image.open("image.png") as image_file:
     print("Successfully loaded image")
-    print(image_file.size)
+    print(image_file.width, image_file.height, "og image size")
+    new_image_width = 622
+    image = image_file.resize((new_image_width, int(new_image_width * (image_file.height / image_file.width))))
+    print(image.size)
     pixel_details = []
-    for y in range(0, image_file.height):
-        for x in range(0, image_file.width):
-            rgb_values = image_file.getpixel((x, y))
+    for y in range(0, image.height):
+        row = []
+        for x in range(0, image.width):
+            rgb_values = image.getpixel((x, y))
             rgb_values_list = []
             for value in rgb_values:
                 rgb_values_list.append(value)
-            pixel_details.append(rgb_values_list)
+            row.append(rgb_values_list)
+        pixel_details.append(row)
     print(pixel_details[0], len(pixel_details))
 
     pixel_brightness_values = []
     for y in range(0, len(pixel_details)):
-        rgb_values = 0
-        for x in pixel_details[y]:
-            rgb_values += x
-        pixel_brightness_values.append(rgb_values/3)
+        row = []
+        for x in range(0, len(pixel_details[y])):
+            rgb_values = 0
+            for value in pixel_details[y][x]:
+                rgb_values += value
+            row.append(int(rgb_values/3))
+        pixel_brightness_values.append(row)
     print(pixel_brightness_values[0], len(pixel_brightness_values))
 
     pixel_ascii_char = []
-    for value in pixel_brightness_values:
-        pixel_ascii_char.append(convert_brightness_to_ascii(value))
-    print(pixel_ascii_char[0], len(pixel_ascii_char))
+    for value in range(0, len(pixel_brightness_values)):
+        row = []
+        for row_value in range(0, len(pixel_brightness_values[value])):
+            row.append(convert_brightness_to_ascii(pixel_brightness_values[value][row_value]))
+        pixel_ascii_char.append(row)
+    print(pixel_ascii_char[0])
+
+    for value in range(0, len(pixel_ascii_char)):
+        for row_value in range(0, len(pixel_ascii_char[value])):
+            print(pixel_ascii_char[value][row_value] * 3, end = "")
+        print()
+        print()
