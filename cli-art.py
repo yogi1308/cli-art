d5f8911a6be4fbd56680a29f55528ed3a304dcaf
Shreetej Hadge <shreetejhadge1308@gmail.com>
2025-11-01 23:19:10 -0700

    added lines of code so that in future the pixel values could be calculated using min-max or luminosity

--- cli-art.py
+++ cli-art.py
@@ -1,4 +1,4 @@
-# add option for brightness using lighness max(R, G, B) + min(R, G, B) / 2 and luminosity 0.21 R + 0.72 G + 0.07 B
+# enable reading terminal height and width to get the ideal width and height for the image
 from PIL import Image
 from colorama import Fore
 
@@ -17,11 +17,8 @@ with Image.open("image.png") as image_file:
     for y in range(0, image.height):
         row = []
         for x in range(0, image.width):
-            rgb_values = image.getpixel((x, y))
-            rgb_values_list = []
-            for value in rgb_values:
-                rgb_values_list.append(value)
-            row.append(rgb_values_list)
+            image.getpixel((x, y))
+            row.append(image.getpixel((x, y)))
         pixel_details.append(row)
     print(pixel_details[0], len(pixel_details))
 
@@ -32,7 +29,9 @@ with Image.open("image.png") as image_file:
             rgb_values = 0
             for value in pixel_details[y][x]:
                 rgb_values += value
-            row.append(int(rgb_values/3))
+            # row.append(int(rgb_values/3)) # average
+            # row.append(int((max(pixel_details[y][x]) + (min(pixel_details[y][x])))/2)) # min-max
+            row.append(int(0.21*pixel_details[y][x][0] + 0.72*pixel_details[y][x][1] + 0.07*pixel_details[y][x][2])) # luminosity
         pixel_brightness_values.append(row)
     print(pixel_brightness_values[0], len(pixel_brightness_values))
 
@@ -44,10 +43,14 @@ with Image.open("image.png") as image_file:
         pixel_ascii_char.append(row)
     print(pixel_ascii_char[0])
 
+    print_this = ""
+
     for value in range(0, len(pixel_ascii_char)):
         for row_value in range(0, len(pixel_ascii_char[value])):
-            print(pixel_ascii_char[value][row_value] * 3, end = "")
+            print_this += f"{pixel_ascii_char[value][row_value] * 3}"
             # print("\033[38;2;11;219;247m" + pixel_ascii_char[value][row_value] * 3, end = "") # prints blue color with rgb(11, 219,247)
             # print(Fore.BLUE + pixel_ascii_char[value][row_value] * 3, end = "")
-        print()
-        print()
+        print_this += "\n\n"
+    print(print_this)
+
+print(max((2, 4, 7)))
